7ff8bbdbd0744e6451a8ffc01082504106f6c324
Greenie115 <dggreen545@gmail.com>
2024-02-04 19:48:46 +0000

    working player name editing

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,42 @@
-function Player({name, symbol}){
-    return(
+import { useState } from "react";
+
+function Player({ name, symbol }) {
+
+    const [editedPlayerName, setEditedPlayerName] = useState(name)
+    const [isEditable, setIsEditable] = useState(false)
+
+    const handleEditPlayer = () => {
+        setIsEditable(editable => !editable)
+    }
+
+    const handleChange = (event) => {
+        setEditedPlayerName(event.target.value)
+    }
+
+    return (
         <>
-        <li>
-        <span className="player-name">{name}</span>
-        <span className="player-symbol">{symbol}</span>
-      </li>
-      <button>Edit</button>
-      </>
+            <li>
+                {isEditable ?
+                    <>
+                        <input
+                            className='player-name'
+                            type="text"
+                            value={editedPlayerName}
+                            onChange={handleChange}
+                            ></input>
+                        <span className="player-symbol">{symbol}</span>
+                    </>
+                    :
+                    <>
+                        <span className="player-name">{editedPlayerName}</span>
+                        <span className="player-symbol">{symbol}</span>
+                    </>
+                }
+            </li>
+            <button
+                onClick={handleEditPlayer}
+            >{isEditable ? 'Save' : 'Edit'}</button>
+        </>
     )
 }
 
