2aac8e0e852eb3b67059030e1c9b84ebe80ed019
Greenie115 <dggreen545@gmail.com>
2024-02-07 19:51:02 +0000

    game over screen and rematch button working

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Log from "./components/Log"
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
+import GameOver from "./Game-Over"
 import { WINNING_COMBINATIONS } from "./Winning_Combinations"
 
 const boardLayout = [
@@ -9,40 +10,50 @@ const boardLayout = [
   [null, null, null],
   [null, null, null],
 ]
-let gameBoard = boardLayout
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X'
-
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, col } = square
-    gameBoard[row][col] = player
-  }
-
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col]
-    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].col]
-    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col]
-
-let winner;
-
-    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol
-      && firstSquareSymbol === thirdSquareSymbol){
-        winner = firstSquareSymbol
-      }
-}
-
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
     currentPlayer = 'O'
   }
   return currentPlayer
 }
 
-function App() {
 
+function App() {
   const [gameTurns, setGameTurns] = useState([])
   const activePlayer = deriveActivePlayer(gameTurns)
+
+  let gameBoard = [...boardLayout.map(array => [...array] )]
+  
+    for (const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square
+      gameBoard[row][col] = player
+    }
+
+    let winner;
+
+for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = 
+    gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol = 
+    gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol = 
+    gameBoard[combination[2].row][combination[2].column]
+    
+    if (firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol && 
+      firstSquareSymbol === thirdSquareSymbol){
+      }
+      winner = firstSquareSymbol
+    }  
+
+    const handleRestart = () => {
+      setGameTurns([])
+    }
+
+    const hasDraw = gameTurns.length === 9 && !winner
 
   function handleActivePlayer(rowIndex, colIndex) {
     setGameTurns(prevTurns => {
@@ -65,9 +76,10 @@ function App() {
           <Player name="Player 1" symbol='X' isActive={activePlayer === 'X'} />
           <Player name='Player 2' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard
           onSelectSquare={handleActivePlayer}
-          gameBoard={gameBoard}
+          board={gameBoard}
            />
       </div>
       <Log turns={gameTurns} />
